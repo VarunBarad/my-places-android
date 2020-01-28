f90f46f1eac4f06db349b48e02948148e9ca729b
Varun Barad <varun@varunbarad.com>
2020-01-28 12:59:49 +0530

    Add repository interface for accessing and saving places

---
+++ app/src/main/java/com/varunbarad/myplaces/repositories/PlacesRepository.kt
@@ -0,0 +1,18 @@
+package com.varunbarad.myplaces.repositories
+
+import com.varunbarad.myplaces.external_services.local_database.model.DbLocation
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+
+interface PlacesRepository {
+    fun insertNewPlace(name: String, comments: String): Single<Long>
+
+    fun getAllPlacesSortedAlphabeticallyByName(): Observable<List<DbLocation>>
+
+    fun getAllPlacesSortedReverseChronologically(): Observable<List<DbLocation>>
+
+    fun getPlaceDetails(locationId: Long): Observable<DbLocation>
+
+    fun updatePlaceDetails(location: DbLocation): Completable
+}
