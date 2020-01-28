7c2abd17e3aa23f938009036d513f1c4c2b2fcf3
Varun Barad <varun@varunbarad.com>
2020-01-28 12:37:41 +0530

    Add a ui-model for location

---
+++ app/src/main/java/com/varunbarad/myplaces/model/UiLocation.kt
@@ -0,0 +1,24 @@
+package com.varunbarad.myplaces.model
+
+import androidx.recyclerview.widget.DiffUtil
+import java.util.*
+
+data class UiLocation(
+    val locationId: Long,
+    val name: String,
+    val comments: String,
+    val timestamp: Date
+) {
+    companion object {
+        @JvmStatic
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<UiLocation>() {
+            override fun areItemsTheSame(oldItem: UiLocation, newItem: UiLocation): Boolean {
+                return (oldItem.locationId == newItem.locationId)
+            }
+
+            override fun areContentsTheSame(oldItem: UiLocation, newItem: UiLocation): Boolean {
+                return (oldItem == newItem)
+            }
+        }
+    }
+}
