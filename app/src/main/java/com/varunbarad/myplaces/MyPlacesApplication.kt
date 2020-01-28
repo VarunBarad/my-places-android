2b4809821d2e63ed537a2d609eebf2b85d9382ae
Varun Barad <varun@varunbarad.com>
2020-01-28 12:40:21 +0530

    Add Joda Time Android library

--- app/src/main/java/com/varunbarad/myplaces/MyPlacesApplication.kt
+++ app/src/main/java/com/varunbarad/myplaces/MyPlacesApplication.kt
@@ -2,10 +2,14 @@ package com.varunbarad.myplaces
 
 import android.app.Application
 import com.varunbarad.myplaces.util.Dependencies
+import net.danlew.android.joda.JodaTimeAndroid
 
 class MyPlacesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        // Initialize Joda Time Android library
+        JodaTimeAndroid.init(this)
 
         // This initializes the notes database
         Dependencies.getNotesDatabase(this)
